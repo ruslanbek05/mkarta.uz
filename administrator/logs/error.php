76be2acc776c63b483f25655907fb54de8507a6d
ruslanbek05 <ruslanbek05@gmail.com>
2020-02-23 09:33:47 +0500

    1

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-02-21 15:18:37 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-02-21T15:18:37+00:00	INFO 94.158.53.9	joomlacanceled	
+2020-02-22T07:11:33+00:00	INFO 94.158.53.9	joomlacanceled	
+2020-02-22T07:16:54+00:00	INFO 94.158.53.9	joomlacanceled	
